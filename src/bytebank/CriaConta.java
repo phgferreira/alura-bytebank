bb28c87adc634d980095c88b15ab309436abc56f
Paulo Ferreira <phgferreira@outlook.com>
2022-08-08 20:36:46 -0300

    Instanciado segunda conta

--- src/bytebank/CriaConta.java
+++ src/bytebank/CriaConta.java
@@ -6,5 +6,14 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeira conta tem " + primeiraConta.saldo);
+		System.out.println("segunda conta tem " + segundaConta.saldo);
 	}
 }
