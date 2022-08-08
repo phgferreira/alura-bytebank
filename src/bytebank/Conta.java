23c2ff19d32bee98328fbdff94405215af3d4de1
Paulo Ferreira <phgferreira@outlook.com>
2022-08-08 20:59:01 -0300

    Demonstração de referência vs objeto

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -3,7 +3,7 @@ package bytebank;
 public class Conta {
 
 	double saldo;
-	int agencia;
+	int agencia = 42;
 	int numero;
 	String titular;
 }
---
+++ src/bytebank/TesteReferencia.java
@@ -0,0 +1,23 @@
+package bytebank;
+
+public class TesteReferencia {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("saldo da primeira: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		segundaConta.saldo += 100;
+		System.out.println("saldo da primeira: " + primeiraConta.saldo);
+		System.out.println("saldo da segunda: " + segundaConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("são a mesmissima conta");
+		} else {
+			System.out.println("contas diferentes");
+		}
+	}
+}
