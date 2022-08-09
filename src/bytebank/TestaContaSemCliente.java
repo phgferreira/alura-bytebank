5875433dbc07309d06bb61775921969fce0430e2
Paulo Ferreira <phgferreira@outlook.com>
2022-08-09 20:20:03 -0300

    Referência Null

---
+++ src/bytebank/TestaContaSemCliente.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+import bytebank.model.Cliente;
+import bytebank.model.Conta;
+
+public class TestaContaSemCliente {
+
+	public static void main(String[] args) {
+		Conta contaDaMarcela = new Conta();
+		System.out.println(contaDaMarcela.saldo);
+		
+		contaDaMarcela.titular = new Cliente();
+		System.out.println(contaDaMarcela.titular);
+
+		contaDaMarcela.titular.nome = "Marcela";
+		System.out.println(contaDaMarcela.titular.nome);
+	}
+}
