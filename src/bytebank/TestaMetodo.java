915991e1a6fecafe4eb133e64b168400dfe08c0f
Paulo Ferreira <phgferreira@outlook.com>
2022-08-08 21:34:20 -0300

    Passando referencias com parametro em método

--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -11,5 +11,16 @@ public class TestaMetodo {
 		boolean conseguiuRetirar = contaDoPaulo.saca(20);
 		System.out.println(contaDoPaulo.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		if (contaDaMarcela.transfere(300, contaDoPaulo)) {
+			System.out.println("Transferencia com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
 }
