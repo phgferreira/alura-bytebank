951d79bd699f9e15cef7af9a3bf25e643f0f5c16
Paulo Ferreira <phgferreira@outlook.com>
2022-08-10 20:31:12 -0300

    Herança

---
+++ src/bytebank/model/Gerente.java
@@ -0,0 +1,20 @@
+package bytebank.model;
+
+public class Gerente extends Funcionario {
+
+	private int senha;
+	
+	public boolean autentica(int senha) {
+		if (this.senha == senha)
+			return true;
+		return false;
+	}
+	
+	/*public double getBonificacao() {
+		return this.salario;
+	}*/
+	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+}
