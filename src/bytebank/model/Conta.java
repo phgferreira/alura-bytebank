106caf687b993003461264b778946e53ad12bd89
Paulo Ferreira <phgferreira@outlook.com>
2022-08-09 21:13:43 -0300

    Construtores

--- src/bytebank/model/Conta.java
+++ src/bytebank/model/Conta.java
@@ -7,6 +7,12 @@ public class Conta {
 	private int numero;
 	private Cliente titular;
 	
+	public Conta(int agencia, int numero) {
+		this.setAgencia(agencia);
+		this.setNumero(numero);
+		System.out.println("estou criando uma conta " + this.numero);
+	}
+	
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
@@ -38,6 +44,10 @@ public class Conta {
 	}
 	
 	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("não pode valor <= 0");
+			return;
+		}
 		this.numero = numero;
 	}
 	
@@ -46,6 +56,10 @@ public class Conta {
 	}
 	
 	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("não pode valor menor ou igual a zero");
+			return;
+		}
 		this.agencia = agencia;
 	}
 	
